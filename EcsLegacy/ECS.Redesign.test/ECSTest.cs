bde3e7d494eeef947f6bcb408b7d18baa3b1278e
Oliver-Schousboe <95352080+Oliver-Schousboe@users.noreply.github.com>
2022-09-29 11:46:43 +0200

    Changed test values to be closer to boundary values and made comments as to redundant tests

--- EcsLegacy/ECS.Redesign.test/ECSTest.cs
+++ EcsLegacy/ECS.Redesign.test/ECSTest.cs
@@ -45,50 +45,67 @@ namespace ECS.Redesign.test
         #endregion
 
         #region RegulateTests
+        //1
         [Test]
         public void Regulate_HighTemp_HeaterTurnedOff()
         {
-            FS.GetTemp().Returns(45);
+            FS.GetTemp().Returns(26);
             uut.Regulate();
             FH.Received(1).TurnOff();
         }
+        //2
         [Test]
         public void Regulate_LowTemp_HeaterTurnedOn()
         {
 
-            FS.GetTemp().Returns(5);
+            FS.GetTemp().Returns(14);
             uut.Regulate();
             FH.Received().TurnOn();
         }
+        //3
         [Test]
         public void Regulate_LowTemp_WindowClosed()
         {
-            FS.GetTemp().Returns(5);
+            FS.GetTemp().Returns(14);
             uut.Regulate();
             FW.Received(1).Close();
         }
+        //4
         [Test]
         public void Regulate_HighTemp_WindowOpened()
         {
-            FS.GetTemp().Returns(45);
+            FS.GetTemp().Returns(26);
             uut.Regulate();
             FW.Received(1).Open();
         }
-        [Test]
-        public void Regulate_MiddleTemp_WindowClosed()
+        //5
+        [TestCase(15)]
+        [TestCase(16)]
+        [TestCase(24)]
+        [TestCase(25)]
+        public void Regulate_MiddleTemp_WindowClosed(int input)
         {
-            FS.GetTemp().Returns(20);
+            FS.GetTemp().Returns(input);
             uut.Regulate();
             FW.Received(1).Close();
         }
-
-        [Test]
-        public void Regulate_MiddleTemp_HeaterTurnedOff()
+        //6
+        [TestCase(15)] //On boundary value
+        [TestCase(16)] //1 above boundary value
+        [TestCase(24)] //1 below boundary valye
+        [TestCase(25)] //on boundary value
+        public void Regulate_MiddleTemp_HeaterTurnedOff(int input)
         {
-            FS.GetTemp().Returns(20);
+            FS.GetTemp().Returns(input);
             uut.Regulate();
             FH.Received(1).TurnOff();
         }
+
+        /*
+         Tests 5 and 6 COULD be combined (when code implementation is known),
+         Tests 2 and 3 COULD be combined (when code implementation is known),
+         Tests 1 and 4 COULD be combined (when code implementation is known)
+        */
         #endregion
 
         #region RunSelfTests
